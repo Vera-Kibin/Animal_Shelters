a39153cac405ba8d7b9715f6cd7dbf5b9905fb14
kravchenski <max5045955@gmail.com>
2026-06-25 16:23:47 +0200

    feat: add cookies banner with privacy policy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./styles/tokens.css";
 import Header from "./components/Layout/Header";
 import Footer from "./components/Layout/Footer";
+import CookieBanner from "./components/CookieBanner/CookieBanner";
 import HomePage from "./pages/HomePage";
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <HomePage />
       </main>
       <Footer />
+      <CookieBanner />
     </>
   );
 }
